fb6d32c1c68e565f8b7083598352464a22b86a6e
SHnice <108930470+SHnice@users.noreply.github.com>
2022-10-24 18:57:57 +0500

    Update Welcome.js

--- src/Welcome.js
+++ src/Welcome.js
@@ -5,10 +5,10 @@ export default function Welcome() {
     <div className='flex justify-center'>
     <div className="container bg-blue-100 shadow border p-4 md:p-6 md:m-8 md:mt-0 md:rounded-xl ">
       <p className="text-2xl text-gray-700 font-bold mb-2">
-        Welcome! I am Talha And My AI Work
+        Welcome! Cats And Dog Classifier
       </p>
       <p className="text-gray-500 text-lg">
-        Detecting Cat or Dog || Deep Learning
+        Image Classification with Transfer Learning
       </p>
     </div>
     </div>
