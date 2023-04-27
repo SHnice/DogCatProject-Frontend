dc667ca0d005124d857453b35dd0f525a6c4d781
Saddam Hussain <108930470+SHnice@users.noreply.github.com>
2023-04-28 04:24:36 +0500

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -36,24 +36,24 @@ export default function Header() {
           </svg>
           <span className="ml-3 text-xl">Saddam Hussain</span>
         </a>
-//         <nav
-//           className="md:ml-auto flex flex-wrap items-center text-base justify-center"
-//         >
+        <nav
+          className="md:ml-auto flex flex-wrap items-center text-base justify-center"
+        >
           
-//           <button
-//           className="inline-flex items-center ml-2 bg-gray-200 font-medium border-0 py-1 px-7 focus:outline-none hover:bg-gray-300 rounded text-base  md:mt-0" onClick={abouutMe}
-//         >
-//           About Me
+          <button
+          className="inline-flex items-center ml-2 bg-gray-200 font-medium border-0 py-1 px-7 focus:outline-none hover:bg-gray-300 rounded text-base  md:mt-0" onClick={abouutMe}
+        >
+          About Me
         
-//         </button>
-//         <button
-//           className="inline-flex items-center mx-4 font-medium bg-gray-200 border-0 py-1 px-7 focus:outline-none hover:bg-gray-300 rounded text-base  md:mt-0" onClick={contactMe}
-//         >
-//           Contact Me
+        </button>
+        <button
+          className="inline-flex items-center mx-4 font-medium bg-gray-200 border-0 py-1 px-7 focus:outline-none hover:bg-gray-300 rounded text-base  md:mt-0" onClick={contactMe}
+        >
+          Contact Me
         
-//         </button>
+        </button>
           
-//         </nav>
+        </nav>
       
       </div>
     </header>
